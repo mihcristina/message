d06d5d913efedb71eb88d7d51f0c0e879cdc425a
Michelli <contato@michellicristina.com.br>
2022-01-20 08:14:52 -0300

    chore: initial config

---
+++ message-app/Login/LoginVC/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  message-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 20/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+        
+    }
+
+
+}
+
